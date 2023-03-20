08e7765683d25df685a96c25b4e6d19f994b13e8
swathisb <swathi.sb16@gmail.com>
2023-03-20 19:04:00 +0530

    function is added to check click

---
+++ script.js
@@ -0,0 +1,14 @@
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+      endGame(false)
+    } else if (isDraw()) {
+      endGame(true)
+    } else {
+      swapTurns()
+      setBoardHoverClass()
+    }
+  }
+  
